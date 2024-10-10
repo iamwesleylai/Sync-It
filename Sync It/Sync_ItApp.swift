ff5b7dd231b4ff4585abad81a827d4b073086242
wesleylaiks <wesleylai.ks@gmail.com>
2024-10-10 17:31:07 -0400

    Initial Commit

---
+++ Sync It/Sync_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sync_ItApp.swift
+//  Sync It
+//
+//  Created by Wesley Lai on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Sync_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sync ItTests/Sync_ItTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sync_ItTests.swift
+//  Sync ItTests
+//
+//  Created by Wesley Lai on 10/10/24.
+//
+
+import Testing
+@testable import Sync_It
+
+struct Sync_ItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
